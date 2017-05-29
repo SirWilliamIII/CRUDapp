bacd4334b2c2ebd937d2ecd705d44e761a5cc43a
Will Carpenter <willcarpenteriii@gmail.com>
2017-05-29 01:19:31 -0600

    able to insert TODO into db

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -8,12 +8,47 @@ router.get('/', (req, res) => {
 	knex('todo')
 	.select()
 	.then(todos => {
-		res.render('all', { todos: todos });
+		res.render('all', {todos: todos});
 	});
 });
 
 router.get('/new', (req, res) => {
 	res.render('new');
+});
+
+function validTodo(todo) {
+	return typeof todo.title == 'string' &&
+	    todo.title.trim() != '' &&
+	    typeof todo.priority == 'number'
+}
+
+router.post('/', (req, res) => {
+
+	console.log(req.body);
+
+	if (validTodo(req.body)) {
+
+		const todo = {
+			title: req.body.title,
+			description: req.body.description,
+			priority: req.body.priority
+		};
+
+		knex('todo')
+		    .insert(todo, 'id')
+		    .then(todos => {
+			    const id = ids[0];
+			    res.redirect(`/todo/${id}`);
+		    });
+
+	    } else {
+
+		res.status(500);
+		res.render('error', {
+			message: 'Invalid TODO'
+		});
+
+	    }
 });
 
 module.exports = router;
