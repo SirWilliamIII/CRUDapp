80ae290ba3143467bb1c164b104aec0aa275ee57
Will Carpenter <willcarpenteriii@gmail.com>
2017-05-29 03:18:02 -0600

    GET /todo/:id/edit

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -6,43 +6,53 @@ router.get('/', (req, res) => {
 	knex('todo')
 	.select()
 	.then(todos => {
-		res.render('all', {todos: todos});
+		res.render('all', { todos: todos });
 	});
+});
+
+router.get('/:id', (req, res) => {
+	const id = req.params.id;
+	if(typeof id != 'undefined') {
+		knex('todo')
+		.select()
+		.where('id', id)
+		.first()
+		.then(todo => {
+			res.render('single', todo);
+		});
+	} else {
+		res.status(500);
+		res.render('error', {
+			message: 'Invalid ID'
+		});
+	}
 });
 
 router.get('/new', (req, res) => {
 	res.render('new');
 });
 
+
 function validTodo(todo) {
 
-	return (
-
-	  typeof todo.title == 'string' &&
-
-	    todo.title.trim() != '' &&
-
-	  typeof todo.priority != 'undefined' &&
-
-	  !isNaN(Number(todo.priority))
-
-	);
+	return typeof todo.title == 'string'
+	  && todo.title.trim() != ''
+	  && typeof todo.priority != 'undefined'
+	  && !isNaN(Number(todo.priority))
 }
 
 router.post('/', (req, res) => {
-	console.log(req.body);
-	if (validTodo(req.body)) {
 
+	if (validTodo(req.body)) {
 		const todo = {
 			title: req.body.title,
 			description: req.body.description,
 			priority: req.body.priority,
 			date: new Date()
 		};
-
 		knex('todo')
 			.insert(todo, 'id')
-			.then(todos => {
+			.then(ids => {
 				const id = ids[0];
 				res.redirect(`/todo/${id}`);
 			});
@@ -54,5 +64,7 @@ router.post('/', (req, res) => {
 
 	}
 });
+
+
 
 module.exports = router;
