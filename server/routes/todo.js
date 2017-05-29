3f357dc29272ae3b6bf83e5884dd2334a502da5d
Will Carpenter <willcarpenteriii@gmail.com>
2017-05-29 01:59:18 -0600

    created new TODO with POST /todo

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 
-/* Make GET req. Router mounted at http://localhost:3000/todo*/
-
 router.get('/', (req, res) => {
 	knex('todo')
 	.select()
@@ -17,38 +15,44 @@ router.get('/new', (req, res) => {
 });
 
 function validTodo(todo) {
-	return typeof todo.title == 'string' &&
+
+	return (
+
+	  typeof todo.title == 'string' &&
+
 	    todo.title.trim() != '' &&
-	    typeof todo.priority == 'number'
+
+	  typeof todo.priority != 'undefined' &&
+
+	  !isNaN(Number(todo.priority))
+
+	);
 }
 
 router.post('/', (req, res) => {
-
 	console.log(req.body);
-
 	if (validTodo(req.body)) {
 
 		const todo = {
 			title: req.body.title,
 			description: req.body.description,
-			priority: req.body.priority
+			priority: req.body.priority,
+			date: new Date()
 		};
 
 		knex('todo')
-		    .insert(todo, 'id')
-		    .then(todos => {
-			    const id = ids[0];
-			    res.redirect(`/todo/${id}`);
-		    });
-
-	    } else {
-
+			.insert(todo, 'id')
+			.then(todos => {
+				const id = ids[0];
+				res.redirect(`/todo/${id}`);
+			});
+	} else {
 		res.status(500);
 		res.render('error', {
 			message: 'Invalid TODO'
 		});
 
-	    }
+	}
 });
 
 module.exports = router;
